c02ddcb18f55a3110e3ff0bcf035087b460d3955
Liz Zhu <77089092+zzhu247@users.noreply.github.com>
2025-11-03 21:46:20 -0500

    Add routes for driver, messages and order

--- dropmate-backend/src/server.js
+++ dropmate-backend/src/server.js
@@ -6,6 +6,9 @@ import { Server } from "socket.io";
 import shipmentRoutes from "./routes/shipments.js";
 import authRoutes from "./routes/auth.js";
 import db from "./models/db.js";
+import orderRoutes from "./routes/orders.js";
+import driverRoutes from "./routes/drivers.js";
+import messageRoutes from "./routes/messages.js";
 
 dotenv.config();
 const app = express();
@@ -15,6 +18,9 @@ app.use(express.json());
 // REST routes
 app.use("/api/shipments", shipmentRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/drivers", driverRoutes);
+app.use("/api/messages", messageRoutes);
 
 // Setup WebSocket
 const httpServer = createServer(app);
