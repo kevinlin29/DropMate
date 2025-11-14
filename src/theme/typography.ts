718c27f1c160737d3188a3c7c71f1b1f9c10207b
Liz Zhu <77089092+zzhu247@users.noreply.github.com>
2025-11-14 11:12:59 -0500

    centralize ALL text definitions

---
+++ src/theme/typography.ts
@@ -0,0 +1,44 @@
+import { StyleSheet } from "react-native";
+
+export const TextStyles = StyleSheet.create({
+  h1: {
+    fontSize: 32,
+    fontFamily: "Inter-Bold",
+    lineHeight: 38,
+  },
+  h2: {
+    fontSize: 28,
+    fontFamily: "Inter-SemiBold",
+    lineHeight: 34,
+  },
+  h3: {
+    fontSize: 22,
+    fontFamily: "Inter-SemiBold",
+    lineHeight: 28,
+  },
+  body: {
+    fontSize: 16,
+    fontFamily: "Inter-Regular",
+    lineHeight: 22,
+  },
+  bodyMedium: {
+    fontSize: 15,
+    fontFamily: "Inter-Medium",
+    lineHeight: 21,
+  },
+  small: {
+    fontSize: 13,
+    fontFamily: "Inter-Regular",
+    lineHeight: 18,
+  },
+  caption: {
+    fontSize: 11,
+    fontFamily: "Inter-Regular",
+    lineHeight: 14,
+  },
+  captionMedium: {
+    fontSize: 12,
+    fontFamily: "Inter-Medium",
+    lineHeight: 16,
+  },
+});
